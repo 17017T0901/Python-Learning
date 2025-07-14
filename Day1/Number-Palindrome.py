c7895c90b5ff1bea4993603ce8796cccce869b80
17017T0901 <55268531+17017T0901@users.noreply.github.com>
2025-07-14 22:44:20 +0530

    Number-Palindrome.py

---
+++ Day1/Number-Palindrome.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = n
+rev = 0
+while n>0:
+    r = n % 10
+    rev = rev * 10 + r
+    n = n // 10
+
+if a == rev:
+    print("The {} value is palindrome".format(a))
+else:
+    print("The {} value is not palindrome".format(a))
+# other way
+n = int(input())
+res = int(str(n)[::-1])
+if n == res:
+    print("{} is palindrome".format(n))
+else:
+    print("{} is not palindrome".format(n))
